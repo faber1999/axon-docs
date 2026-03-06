1bba4912e64f4309e7851cc5defdca6aa944304c
Faber Grajales <fabergrajales1999@gmail.com>
2026-03-06 01:33:42 -0500

    feat: enhance App component with routing effect and scroll reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { createApp, createRouter, RouterView } from '@faber1999/axon.js'
+import { createApp, createRouter, effect, RouterView, useRouter } from '@faber1999/axon.js'
 
-import { routes } from './router/routes.ts'
 import { Navbar } from './components/Navbar'
-import './stores/themeStore'  // initialize theme effect on startup
+import { routes } from './router/routes.ts'
+import './stores/themeStore' // initialize theme effect on startup
 import './style.css'
 
 createRouter(routes, { viewTransitions: true })
 
 function App() {
+  const router = useRouter()
+
+  effect(() => {
+    router.pathname()
+    window.scrollTo(0, 0)
+  })
+
   return (
     <div class="app">
       <Navbar />
